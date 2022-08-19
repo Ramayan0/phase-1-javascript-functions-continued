c660819376c11a3b61b3c1b9a9a45d3f78fa9e23
hamsa <hafieekhan@gmail.com>
2022-08-19 10:17:00 +0300

    Complted Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return(`This Saturday, I want to ${activity}!`);
+  }
+  // saturdayFun(); 
+  // saturdayFun("bathe my dog");
+
+  const mondayWork = function(act ="go to the office"){
+    return(`This Monday, I will ${act}.`);
+  }
+  // mondayWork();
+  // mondayWork("work from home");
+
+function wrapAdjective(symbol){
+   return function(adjective){
+    return `You are ${symbol}${adjective}${symbol}!`;
+   }
+    }
